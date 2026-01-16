32fe7adafd10a800f198889861fe27be77c68679
Cole Grundy <cole@087558.com>
2026-01-16 06:08:56 -0500

    init

---
+++ refinery.php
@@ -0,0 +1,15 @@
+<?php
+namespace beryllium;
+
+use Laminas\Diactoros\Response;
+use Laminas\Diactoros\Response\RedirectResponse;
+use League\Flysystem\Adapter\Local;
+use League\Flysystem\Filesystem;
+use League\Pipeline\Pipeline;
+use League\Pipeline\PipelineBuilder;
+
+class Refinery {
+
+}
+
+//... better hydrator, etc
